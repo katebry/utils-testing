5c7b4bc393d5a9a01f5a1a9e0f0d04ddcb97b859
Kate Bryan <katebryan31@gmail.com>
2021-03-30 20:36:55 +0100

    feat: added capitalise function and tests

--- formatters/stringFormatter.js
+++ formatters/stringFormatter.js
@@ -1,6 +1,13 @@
 function addSpacesToCamelCase(string) {
   if (!string) return false;
-  return string.replace(/([A-Z](?=[a-z]+)|[A-Z]+(?![a-z]))/g, ' $1')
+  return string.replace(/([A-Z](?=[a-z]+)|[A-Z]+(?![a-z]))/g, " $1");
 }
 
-module.exports = { addSpacesToCamelCase };
+function capitalise(data) {
+  if (!data) return false;
+  return data
+    .toLowerCase()
+    .replace(/(^\w{1})|(\s{1}\w{1})/g, (match) => match.toUpperCase());
+}
+
+module.exports = { addSpacesToCamelCase, capitalise };
--- formatters/stringFormatter.test.js
+++ formatters/stringFormatter.test.js
@@ -1,7 +1,16 @@
-const { addSpacesToCamelCase } = require("./stringFormatter");
+const { addSpacesToCamelCase, capitalise } = require("./stringFormatter");
 
 test("Adds spaces to a camelCased string", () => {
   expect(addSpacesToCamelCase("")).toBe(false);
   expect(addSpacesToCamelCase("helloWorld")).toBe("hello World");
-  expect(addSpacesToCamelCase("helloWorldItsMeMario")).toBe('hello World Its Me Mario')
+  expect(addSpacesToCamelCase("helloWorldAndGoodbye")).toBe(
+    "hello World And Goodbye"
+  );
+});
+
+test("Capitalises the first letter of each word", () => {
+  expect(capitalise("")).toBe(false);
+  expect(capitalise("HELLO")).toBe("Hello");
+  expect(capitalise("HELLO world")).toBe("Hello World");
+  expect(capitalise("hello world aND GoODBye")).toBe("Hello World And Goodbye");
 });
