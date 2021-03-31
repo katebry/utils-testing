446e698a7c5e2230f80c1823f423f72275bbf99c
Kate Bryan <katebryan31@gmail.com>
2021-03-31 07:05:52 +0100

    chore: refactor

--- generalRegex/nameChecker.js
+++ generalRegex/nameChecker.js
@@ -1,21 +1,15 @@
 function validFirstName(value) {
-  if (value === "") {
-    return false;
-  } else {
-    const firstNameRegex = /^([a-zA-Z]?|[a-z]+[-]?)+$/;
-    const result = firstNameRegex.test(value);
-    return result;
-  }
+  if (!value) return false;
+  const firstNameRegex = /^([a-zA-Z]?|[a-z]+[-]?)+$/;
+  const result = firstNameRegex.test(value);
+  return result;
 }
 
 function validLastName(value) {
-  if (value === "") {
-    return false;
-  } else {
-    const lastNameRegex = /^([a-zA-Z]+[ ']?|[a-z]+[-']?)+$/;
-    const result = lastNameRegex.test(value);
-    return result;
-  }
+  if (!value) return false;
+  const lastNameRegex = /^([a-zA-Z]+[ ']?|[a-z]+[-']?)+$/;
+  const result = lastNameRegex.test(value);
+  return result;
 }
 
 module.exports = { validFirstName, validLastName };
