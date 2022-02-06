53f930807f9cda2a7dc2a034e1ddbd9adf36528f
katebry <katebryan31@gmail.com>
2022-02-05 20:36:46 -0800

    feat: savingsPlanner function and tests passing

--- algorithms/savingsPlanner.js
+++ algorithms/savingsPlanner.js
@@ -1,5 +1,22 @@
 function savingsPlanner(startPriceOfOldCar, startPriceOfNewCar, savingPerMonth, percentLossByMonth) {
-    return [0, percentLossByMonth]
+    let total = 0;
+    let savings = 0;
+    let months = 1;
+
+    let oldCarTotal = startPriceOfOldCar;
+    let newCarTotal = startPriceOfNewCar;
+
+    if (startPriceOfOldCar === startPriceOfNewCar) return [0, 0]
+    if (startPriceOfOldCar > startPriceOfNewCar) return [0, (startPriceOfOldCar - startPriceOfNewCar)]
+
+    for (months; (total + oldCarTotal) < newCarTotal; months++) {
+        if (months % 2 === 0) percentLossByMonth += 0.5;
+        oldCarTotal = percentageDecrease(percentLossByMonth, oldCarTotal)
+        newCarTotal = percentageDecrease(percentLossByMonth, newCarTotal)
+        total += savingPerMonth;
+        savings = (total + oldCarTotal) - newCarTotal;
+    }
+    return [months - 1, Math.round(savings)]
 }
 
 function percentageDecrease(percentage, total) {
@@ -8,4 +25,3 @@ function percentageDecrease(percentage, total) {
 }
 
 module.exports = {savingsPlanner, percentageDecrease};
-
--- algorithms/savingsPlanner.test.js
+++ algorithms/savingsPlanner.test.js
@@ -6,6 +6,11 @@ test('Returns the total value after subtracting the percentage decrease', () => 
     expect(percentageDecrease(5, 8000)).toBe(7600);
 });
 
+test('If the starting price of the old car matches that of the new car, the 0 result is returned', () => {
+    expect(savingsPlanner(8000, 8000, 1000, 1.5)).toEqual([0, 0])
+});
+
 test('Returns the savingsPlanner result as an array of two values', () => {
-    expect(savingsPlanner(8000, 8000, 1000, 1.5)).toStrictEqual([0, 1.5])
+    expect(savingsPlanner(12000, 8000, 1000, 1.5)).toEqual([0, 4000])
+    expect(savingsPlanner(2000, 8000, 1000, 1.5)).toEqual([6, 766])
 });
