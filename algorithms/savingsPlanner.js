7a179afc7aea5c1223ddcf19ee4fc00e16a03136
katebry <katebryan31@gmail.com>
2022-02-05 20:40:38 -0800

    feat: savingsPlanner refactor

--- algorithms/savingsPlanner.js
+++ algorithms/savingsPlanner.js
@@ -1,27 +1,26 @@
 function savingsPlanner(startPriceOfOldCar, startPriceOfNewCar, savingPerMonth, percentLossByMonth) {
-    let total = 0;
     let savings = 0;
-    let months = 1;
+    let months = 0;
 
     let oldCarTotal = startPriceOfOldCar;
     let newCarTotal = startPriceOfNewCar;
 
-    if (startPriceOfOldCar === startPriceOfNewCar) return [0, 0]
-    if (startPriceOfOldCar > startPriceOfNewCar) return [0, (startPriceOfOldCar - startPriceOfNewCar)]
-
-    for (months; (total + oldCarTotal) < newCarTotal; months++) {
-        if (months % 2 === 0) percentLossByMonth += 0.5;
-        oldCarTotal = percentageDecrease(percentLossByMonth, oldCarTotal)
-        newCarTotal = percentageDecrease(percentLossByMonth, newCarTotal)
-        total += savingPerMonth;
-        savings = (total + oldCarTotal) - newCarTotal;
+    while (startPriceOfNewCar > startPriceOfOldCar + savings) {
+        savings += savingPerMonth;
+        oldCarTotal = percentageDecrease(percentLossByMonth, oldCarTotal);
+        newCarTotal = percentageDecrease(percentLossByMonth, newCarTotal);
+        months++;
+        if (months % 2 === 1) {
+            percentLossByMonth += 0.5;
+        }
     }
-    return [months - 1, Math.round(savings)]
+    return [months, Math.round(oldCarTotal + savings - newCarTotal)];
+
 }
 
 function percentageDecrease(percentage, total) {
-    const totalToSubtract = (percentage / 100) * total
-    return total - totalToSubtract
+    const totalToSubtract = (percentage / 100) * total;
+    return total - totalToSubtract;
 }
 
 module.exports = {savingsPlanner, percentageDecrease};
--- algorithms/savingsPlanner.test.js
+++ algorithms/savingsPlanner.test.js
@@ -7,10 +7,14 @@ test('Returns the total value after subtracting the percentage decrease', () => 
 });
 
 test('If the starting price of the old car matches that of the new car, the 0 result is returned', () => {
-    expect(savingsPlanner(8000, 8000, 1000, 1.5)).toEqual([0, 0])
+    expect(savingsPlanner(8000, 8000, 1000, 1.5)).toEqual([0, 0]);
 });
 
+test('If the starting price of the old car is greater than that of the new, the savings profit is returned', () => {
+    expect(savingsPlanner(12000, 8000, 1000, 1.5)).toEqual([0, 4000]);
+})
+
 test('Returns the savingsPlanner result as an array of two values', () => {
-    expect(savingsPlanner(12000, 8000, 1000, 1.5)).toEqual([0, 4000])
-    expect(savingsPlanner(2000, 8000, 1000, 1.5)).toEqual([6, 766])
+    expect(savingsPlanner(2000, 8000, 1000, 1.5)).toEqual([6, 766]);
+    expect(savingsPlanner(1000, 8000, 1000, 1.5)).toEqual([7, 1077]);
 });
