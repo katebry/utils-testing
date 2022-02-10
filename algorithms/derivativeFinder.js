8aa6b153ba2dcc4f50be87c8dd5823c39f629756
katebry <katebryan31@gmail.com>
2022-02-09 19:02:45 -0800

    fix: removed derivativeFinder function and tests

--- algorithms/derivativeFinder.js
+++
@@ -1,9 +0,0 @@
-function derivativeFinder(value) {
-    if (!value) return false;
-    if (!value.includes('x')) return '0';
-    else {
-        // other stuff
-    }
-}
-
-module.exports = derivativeFinder;
--- algorithms/derivativeFinder.test.js
+++
@@ -1,9 +0,0 @@
-const derivativeFinder = require("./derivativeFinder");
-
-test('If no value is passed in, the function returns false', () => {
-    expect(derivativeFinder("")).toBe(false);
-});
-
-test('If the string contains a number with no "x" value, return 0', () => {
-    expect(derivativeFinder("42")).toBe('0');
-});
