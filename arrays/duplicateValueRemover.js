553fc2afca4fed7df84618836c3cfe18a61669a0
katebry <katebryan31@gmail.com>
2022-02-09 20:02:47 -0800

    feat: added duplicateValueRemover functionality and tests

--- arrays/duplicateValueRemover.js
+++ arrays/duplicateValueRemover.js
@@ -1,6 +1,6 @@
 function duplicateValueRemover(array1, array2) {
     if (array2.length <= 0) return array1;
-    return array1
+    return array1.filter(val => !array2.includes(val));
 }
 
 module.exports = duplicateValueRemover;
--- arrays/duplicateValueRemover.test.js
+++ arrays/duplicateValueRemover.test.js
@@ -2,8 +2,14 @@ const duplicateValueRemover = require("./duplicateValueRemover");
 
 test('If the second array passed into the function is empty, return the value of array1', () => {
     expect(duplicateValueRemover([], [])).toEqual([]);
-})
+});
 
 test('If the first array passed into the function is empty, return the empty value', () => {
     expect(duplicateValueRemover([], [1, 2])).toEqual([]);
-})
+});
+
+test('If a duplicate value is found in array2, this is removed from the first array and then returned', () => {
+    expect(duplicateValueRemover([1, 3], [1, 2])).toEqual([3]);
+    expect(duplicateValueRemover([1, 8, 2], [1, 2])).toEqual([8]);
+    expect(duplicateValueRemover([1, 2, 3], [1, 3])).toEqual([2]);
+});
