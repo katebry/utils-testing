3ebebef6ccc4fa77181cab66a51b31eda1a1efba
katebry <katebryan31@gmail.com>
2022-02-09 17:53:43 -0800

    feat: derivativeFinder test for non-x value

--- algorithms/derivativeFinder.js
+++ algorithms/derivativeFinder.js
@@ -1,5 +1,9 @@
 function derivativeFinder(value) {
     if (!value) return false;
+    if (!value.includes('x')) return '0';
+    else {
+        // other stuff
+    }
 }
 
 module.exports = derivativeFinder;
