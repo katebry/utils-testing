e8b0dd869fcd52f95234f2e852fc463c62a52cc2
katebry <katebryan31@gmail.com>
2022-02-11 21:06:34 -0800

    feat: updated test cases

--- algorithms/anagramFinder.test.js
+++ algorithms/anagramFinder.test.js
@@ -1,9 +1,13 @@
 const anagramFinder = require("./anagramFinder")
 
 test("If the value passed to the function is empty, an empty array is returned", () => {
-    expect(anagramFinder()).toEqual([]);
+    expect(anagramFinder()).toStrictEqual([]);
 })
 
 test("If the word passed in matches the single word in the array, this is returned", () => {
-    expect(anagramFinder('abba', ['abba'])).toEqual(['abba']);
+    expect(anagramFinder('abba', ['abba'])).toStrictEqual(['abba']);
+})
+
+test("If the word passed in matches one of the words in the array, this is returned", () => {
+    expect(anagramFinder('abba', ['kate', 'bake', 'abba'])).toStrictEqual(['abba']);
 })
