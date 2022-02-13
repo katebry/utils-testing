6d79ad56ab900cffce68b653e64623c7a87857f4
katebry <katebryan31@gmail.com>
2022-02-13 13:23:26 -0800

    chore: testing if pipelines fail

--- formatters/stringFormatter.js
+++ formatters/stringFormatter.js
@@ -1,31 +1,31 @@
 function capitalise(data) {
-  if (!data) return false;
-  return data
-    .toLowerCase()
-    .replace(/(^\w{1})|(\s{1}\w{1})/g, (match) => match.toUpperCase());
+    if (!data) return false;
+    return data
+        .toLowerCase()
+        .replace(/(^\w{1})|(\s{1}\w{1})/g, (match) => match.toUpperCase());
 }
 
 function capitaliseFirstLetter(string) {
-  if (!string) return false;
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    if (!string) return false;
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function addSpacesToCamelCase(string) {
-  if (!string) return false;
-  return string.replace(/([A-Z](?=[a-z]+)|[A-Z]+(?![a-z]))/g, " $1");
+    if (!string) return false;
+    return string.replace(/([A-Z](?=[a-z]+)|[A-Z]+(?![a-z]))/g, " $1");
 }
 
 function toKebabCase(string) {
-  if (!string) return false;
-  return string
-    .replace(/([a-z])([A-Z])/g, "$1-$2")
-    .replace(/\s+/g, "-")
-    .toLowerCase();
+    if (!string) return false;
+    return string
+        // .replace(/([a-z])([A-Z])/g, "$1-$2")
+        .replace(/\s+/g, "-")
+        .toLowerCase();
 }
 
 module.exports = {
-  capitalise,
-  capitaliseFirstLetter,
-  addSpacesToCamelCase,
-  toKebabCase,
+    capitalise,
+    capitaliseFirstLetter,
+    addSpacesToCamelCase,
+    toKebabCase,
 };
