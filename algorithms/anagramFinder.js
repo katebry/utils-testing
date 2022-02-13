656ef007c912dfb89d35766a7fefc71a4b65784e
katebry <katebryan31@gmail.com>
2022-02-13 08:01:51 -0800

    chore: pseudocode added

--- algorithms/anagramFinder.js
+++ algorithms/anagramFinder.js
@@ -1,6 +1,23 @@
 function anagramFinder(word, words) {
     if (!word) return []
-    return words.filter(val => val === word);
+    let indexArray = []
+    let sortedWord = sorter(word)
+    // sort each of the words in the array by a-b
+    // sort the word to match in the array by a-b
+    // run through the array and return the index value of each match
+    // return the filtered original matching (non-sorted) values as an array
+    // return words.filter(val => val === word);
+
+    words.forEach((val, index) => {
+        let sortedVal = sorter(val)
+        console.log(sortedVal)
+        if (sortedVal === sortedWord) return indexArray.push(index)
+    })
+    console.log(indexArray)
+}
+
+function sorter(string) {
+    return string.split('').sort().join('');
 }
 
 module.exports = anagramFinder;
--- algorithms/anagramFinder.test.js
+++ algorithms/anagramFinder.test.js
@@ -8,6 +8,6 @@ test("If the word passed in matches the single word in the array, this is return
     expect(anagramFinder('abba', ['abba'])).toStrictEqual(['abba']);
 })
 
-test("If the word passed in matches one of the words in the array, this is returned", () => {
-    expect(anagramFinder('abba', ['kate', 'bake', 'abba'])).toStrictEqual(['abba']);
+test("If the word passed in is an anagram of one of the words in the array, this is returned", () => {
+    expect(anagramFinder('abba', ['kate', 'bake', 'baba'])).toStrictEqual(['abba']);
 })
