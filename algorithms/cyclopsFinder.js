d178762bbe7f6f3287f47ed802307b6f07321f94
katebry <katebryan31@gmail.com>
2023-01-04 08:20:12 +0000

    Tests and logic written for Cyclops kata

--- algorithms/cyclopsFinder.js
+++ algorithms/cyclopsFinder.js
@@ -1,5 +1,11 @@
-function cyclopsFinder() {
-    return false;
+function cyclopsFinder(value) {
+    if (!value) return false;
+    const binary = value.toString(2);
+    if (+binary === 1) return false;
+    let numberOfZeroes = binary.match(/0/g);
+    if (numberOfZeroes && numberOfZeroes.length > 1) return false;
+    const middleChar = binary.substr(Math.ceil(binary.length / 2 - 1), binary.length % 2 === 0 ? 2 : 1);
+    return (+middleChar === 0);
 }
 
 module.exports = cyclopsFinder;
--- algorithms/cyclopsFinder.test.js
+++ algorithms/cyclopsFinder.test.js
@@ -3,3 +3,18 @@ const cyclopsFinder = require("./cyclopsFinder")
 test("If the value passed to the function is empty, false is returned", () => {
     expect(cyclopsFinder()).toBe(false);
 })
+
+test("If the value passed to the function is a cyclops number, true is returned", () => {
+    expect(cyclopsFinder(5)).toBe(true);
+    expect(cyclopsFinder(27)).toBe(true);
+    expect(cyclopsFinder(2015)).toBe(true);
+})
+
+test("If the value passed to the function is not a cyclops number, false is returned", () => {
+    expect(cyclopsFinder(1)).toBe(false);
+    expect(cyclopsFinder(666)).toBe(false);
+    expect(cyclopsFinder(13)).toBe(false);
+    expect(cyclopsFinder(3)).toBe(false);
+    expect(cyclopsFinder(23)).toBe(false);
+
+})
